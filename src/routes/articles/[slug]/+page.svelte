fec1a8fde9df9610bdb92bca9de051aca56b2ef6
worakrit <88820188+worakritnext@users.noreply.github.com>
2023-08-16 09:55:25 +0700

    markdown tailwind

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -7,6 +7,7 @@
 <div class="h1 text-4xl text-center shadow-lg m-4 text-indigo-600">
 	{data.article.title}
 </div>
-<div class="m-4 text-xl">
+<div class="m-4 text-xl prose lg:prose-xl">
+	<h1>{data.article.title}</h1>
 	{@html data.article.content}
 </div>
