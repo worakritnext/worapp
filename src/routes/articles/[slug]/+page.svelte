ea4b721c64f5f82a16f10b3d9012d72d7ff04846
worakrit <88820188+worakritnext@users.noreply.github.com>
2023-08-16 09:49:21 +0700

    Update +page.svelte
    indigo

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -4,10 +4,9 @@
 	console.log(data.article);
 </script>
 
-<div class="h1 text-4xl text-center">
+<div class="h1 text-4xl text-center shadow-lg m-4 text-indigo-600">
 	{data.article.title}
 </div>
 <div class="m-4 text-xl">
 	{@html data.article.content}
-
 </div>
